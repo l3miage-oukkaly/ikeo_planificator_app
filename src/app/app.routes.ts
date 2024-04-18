2b60b9c0510b2945788fecb754b389b380eadf41
l3miage-farinaa <alessandro.farina75005@gmail.com>
2024-04-18 22:00:37 +0200

    Implemented views (partially) & changed 2 models
    Changed 2 models taking into account Yacinos previous feedback and after reviewing the code on my end and encountering some troubles :
    Added distanceToCover as an optionnal attribute of Delivery and changed deliveries attribute of DeliveryTour to Delivery[] instead for better workflow.
    
    Added the views required for the Golem version of the app. They're not yet finished but everything that is pushed is working.
    
    When finished, there will be some testing to do regarding the endpoints and the localhost environment variable will be updated.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {HomepageComponent} from "./views/homepage/homepage.component";
+import {DayDisplayerComponent} from "./views/day-displayer/day-displayer.component";
+import {DayPlannerComponent} from "./views/day-planner/day-planner.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', component: HomepageComponent},
+  {path:'day-planner', component:DayPlannerComponent},
+  {path:'day-displayer', component:DayDisplayerComponent}
+];
--- src/app/shared/services/planificator.service.ts
+++ src/app/shared/services/planificator.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
+import {inject, Injectable, signal} from '@angular/core';
 import {
   PlanificatorProtocolsImplementation
 } from "../../core/adapters/planificator-protocols/planificator-protocols-implementation";
 import {Day} from "../../core/models/day.models";
+import {SetupBundle} from "../../core/models/setup-bundle.models";
+import {Subject} from "rxjs";
+import {DatePipe} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlanificatorService {
   private planificatorProtocols: PlanificatorProtocolsImplementation = new PlanificatorProtocolsImplementation()
+  private datePipe = inject(DatePipe)
+  currentDate = (new Date)
+  tomorrowDate = this.datePipe.transform(this.currentDate.setDate(this.currentDate.getDate() + 1), 'yyyy-MM-dd')
 
-  constructor() { }
+  constructor() {}
 
   async getSetupBundle() {
     return await this.planificatorProtocols.getSetupBundle()
