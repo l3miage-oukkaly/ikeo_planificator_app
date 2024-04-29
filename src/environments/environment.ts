80aacc1e2374f95ad33de316c173d009bb75a5c7
l3miage-oukkaly <156008078+l3miage-oukkaly@users.noreply.github.com>
2024-04-29 12:03:39 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  localhostUrl : 'http://localhost:8081',
+  localhostUrl : 'http://localhost:8080',
   getSetupBundleUrl : '/api/v1.0/day/bundle',
   postDayUrl : '/api/v1.0/day/plan',
   getDayUrl : '/api/v1.0/day'
